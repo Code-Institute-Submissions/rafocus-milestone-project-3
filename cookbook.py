2e9c2a5756159f6a9836e1fc5d8431147c9198bb
rafocus <rafikar@protonmail.com>
2019-06-28 22:20:51 +0100

    display sample recipes with template

--- cookbook.py
+++ cookbook.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+recipes = [
+    {
+        'name': 'recipe 1',
+        'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, rem?',
+        'method': ['step1', 'step 2', 'step 3'],
+        'picture': 'https://dummyimage.com/300' 
+    },
+    {
+        'name': 'recipe 2',
+        'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, rem?',
+        'method': ['step1', 'step 2', 'step 3'],
+        'picture': 'https://dummyimage.com/300' 
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', recipes=recipes)
 
 @app.route("/about")
 def about():
