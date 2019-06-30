39098941b012a19d72ba2f61393005630e167ddf
rafocus <rafikar@protonmail.com>
2019-06-30 01:06:02 +0100

    register, login forms and templates

--- cookbook.py
+++ cookbook.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0987ads0f987asd0987a0s9d8fa0s98d'
 
 recipes = [
     {
@@ -16,6 +22,8 @@ recipes = [
     }
 ]
 
+# routes
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -24,6 +32,32 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+# registration form fields
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=15)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+# login form fields
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=15)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
 
 if __name__ == '__main__':
     app.run(debug=True)
