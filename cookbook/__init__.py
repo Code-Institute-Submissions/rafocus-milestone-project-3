c028cd4868131e82ae2c62c75450bb4d4826f14f
rafocus <rafikar@protonmail.com>
2019-07-11 10:03:54 +0100

    update databse with migrate

--- cookbook/__init__.py
+++ cookbook/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
@@ -9,6 +10,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 #  we use sqlite for development but we use postgresql for production
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
